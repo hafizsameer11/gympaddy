1d20ad9d38dc4a83902a0a2964fc863341dc2d77
CodeCraftersgcuf <rubab@gmail.com>
2025-06-30 03:27:08 +0500

    Added the hardcoded the APP_ID

--- config/services.php
+++ config/services.php
@@ -31,7 +31,7 @@ return [
     ],
 
     'agora' => [
-        'app_id' => env('AGORA_APP_ID', '2fae578d9eef4fe19df335eb67227571'),
+        'app_id' => env('2fae578d9eef4fe19df335eb67227571', '2fae578d9eef4fe19df335eb67227571'),
         'certificate' => env('AGORA_APP_CERTIFICATE'),
     ],
 ];
