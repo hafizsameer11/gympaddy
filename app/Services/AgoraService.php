2135d2fed5584059adf2a4f6edb4c197a7b7e46b
CodeCraftersgcuf <rubab@gmail.com>
2025-06-30 10:02:47 +0500

    Added the Live Streaming.

--- app/Services/AgoraService.php
+++ app/Services/AgoraService.php
@@ -6,20 +6,38 @@ use TaylanUnutmaz\AgoraTokenBuilder\RtcTokenBuilder;
 
 class AgoraService
 {
+    protected string $appId = '2fae578d9eef4fe19df335eb67227571';
+    protected string $appCertificate = '118e704beaea42e38b74b21a08bded63';
+
     public function generateRtcToken(string $channelName, int $uid, int $expireSeconds = 1800): string
-{
-    $appId = '2fae578d9eef4fe19df335eb67227571';
-    $appCertificate = '118e704beaea42e38b74b21a08bded63';
-    $expireTime = time() + $expireSeconds;
+    {
+        $expireTime = time() + $expireSeconds;
 
-    return RtcTokenBuilder::buildTokenWithUid(
-        $appId,
-        $appCertificate,
-        $channelName,
-        $uid,
-        RtcTokenBuilder::RolePublisher,
-        $expireTime
-    );
-}
+        return RtcTokenBuilder::buildTokenWithUid(
+            $this->appId,
+            $this->appCertificate,
+            $channelName,
+            $uid,
+            RtcTokenBuilder::RolePublisher,
+            $expireTime
+        );
+    }
 
+    public function generateRtcTokenWithRole(string $channelName, int $uid, string $role = 'host', int $expireSeconds = 1800): string
+    {
+        $expireTime = time() + $expireSeconds;
+
+        $roleId = $role === 'host'
+            ? RtcTokenBuilder::RolePublisher   // for broadcasters
+            : RtcTokenBuilder::RoleSubscriber; // for audience
+
+        return RtcTokenBuilder::buildTokenWithUid(
+            $this->appId,
+            $this->appCertificate,
+            $channelName,
+            $uid,
+            $roleId,
+            $expireTime
+        );
+    }
 }
